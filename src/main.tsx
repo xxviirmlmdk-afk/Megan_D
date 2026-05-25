f05d1bb099570337b8063162583b40e825b191c7
David Megan <xxviirmlmdk@gmail.com>
2026-05-25 20:28:52 +0200

    Wrap App in AuthProvider to fix useAuth error

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // optional global styles
+﻿import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { AuthProvider } from './context/AuthContext' // adjust path if needed
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
-);
+)
